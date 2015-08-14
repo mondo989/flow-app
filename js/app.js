fcd0e641076b9aea20e1d5dba8ce0c2981c89d5d
twincharged <joseph.cs.ritchie@gmail.com>
2015-08-13 18:48:30 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,22 +5,18 @@ app.config(function($stateProvider, $urlRouterProvider) {
     .otherwise('/main');
   $stateProvider
     .state("main", {
-      title: "Main Page",
       url: "/main",
       templateUrl: "templates/main.html"
     })
     .state("search", {
-      title: "Search for Assets",
       url: "/search",
       templateUrl: "templates/search.html"
     })
     .state("bottom-carousel", {
-      title: "Bottom Carousel",
       url: "/bottom-carousel",
       templateUrl: "templates/bottom-carousel.html"
     })
     .state("intro", {
-      title: "Login Signup",
       url: "/intro",
       templateUrl: "templates/intro.html"
     })
